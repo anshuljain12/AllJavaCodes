f593183c7f46268c7d8b72db881074093f74ea18
Anshul Jain <anshul.jain1207@gmail.com>
2015-09-09 11:53:47 -0700

    Update PathToAllLeaves.java

--- Trees/PathToAllLeaves.java
+++ Trees/PathToAllLeaves.java
@@ -50,4 +50,21 @@ public class PathToAllLeaves {
 		}
 		return false;
 	}
+	public static List<String> binaryTreePaths(TreeNode root) {
+		List<String> res = new LinkedList<>();
+
+	        if(root == null) 
+	        	return res;
+	
+	        if(root.left == null && root.right == null){
+	            res.add(root.val+"");
+	            return res;
+	        }
+	        for (String path : binaryTreePaths(root.left))
+	            res.add(root.val + "->" + path);
+	        for (String path : binaryTreePaths(root.right))
+	            res.add(root.val + "->" + path);
+	        
+	        return res;
+	}
 }
